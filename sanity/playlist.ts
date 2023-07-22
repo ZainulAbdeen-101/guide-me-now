78aa54f10b32571bb151a05cec6ae9460f8e40de
arslanarif9090 <138647677+arslanarif9090@users.noreply.github.com>
2023-07-22 14:22:39 +0500

    playlist schema

---
+++ sanity/playlist.ts
@@ -0,0 +1,29 @@
+export const playlist={
+
+
+name:"playlist",
+type :"document",
+title: "yt course",
+
+fields:[
+
+{
+    name:"url",
+    type:"string",
+    title:"url"
+}
+,
+{
+    name :"heading",
+    type:"string",
+    title:"heading"
+},
+{
+    name :"title",
+    type:"string",
+    title:"video title"
+}
+
+]
+
+}
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,9 +1,10 @@
 import {type SchemaTypeDefinition } from 'sanity'
 import { data } from './frontend_basic'
+import { playlist } from './playlist'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [data],
+  types: [data,playlist],
 }
 
 
