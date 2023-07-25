1126a8bddd5cf5fe4a240bcecbc4645cb7ec43e2
shayanalam321 <138647695+shayanalam321@users.noreply.github.com>
2023-07-25 15:34:49 +0500

    playlist page completed

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -9,22 +9,30 @@ props : string
 
 }
 
+interface Playlist{
+
+heading:string,
+url:string,
+title:string,
+id:string  
+  }
+
 export default function PlayList({props}:Play) {
   
-
-  const [data, setData] = useState([]);
+const [show,setshow]=useState(true)
+  const [data, setData] = useState<Playlist[]|undefined>([]);
   const [list, setList] = useState<string>("");
 
   function setdata(url: string) {
     setList(url);
   }
-
+  
   function compareByProperty(property: any) {
     return (a: string, b: string) =>
       a[property].toUpperCase().localeCompare(b[property].toUpperCase());
   }
 
-  useEffect(() => {
+   useEffect(() => {
     async function fetchData() {
       const play = props;
       const fetchedPlaylist = await fetchplaylist(play);
@@ -36,17 +44,29 @@ export default function PlayList({props}:Play) {
     fetchData();
   },[props]);
 
+
+  function showbtn(title:string) {
+    
+    if (data && title === data[data.length-1].title) {
+      setshow(false);
+    }
+  }
+
   return (
     <>
+   
     <div className="flex justify-around">
 
 {/* #fed32e */}
           
+        
 
-      <div className="max-w-[450px] flex flex-col h-[600px] gap-10 text-lg border rounded-lg overflow-auto scrollbar-thin scrollbar-thumb-[#6c00a5] scrollbar-track-[#fed32e] bg-gradient-to-br from-gray-100 to-gray-300">
-        {data.map((item: playlist<string>, index: number) => (
-          <div className=" m-2 shadow-xl shdaow rounded-md" key={index}>
-            <button onClick={() => setdata(item.url)}>{item.title}</button>
+      <div className="mt-10 max-w-[450px] flex flex-col h-[600px] gap-10 text-lg border rounded-lg overflow-auto scrollbar-thin scrollbar-thumb-[#6c00a5] scrollbar-track-[#fed32e]">
+        {data?.map((item: playlist<string>, index: number) => (
+          <div className=" m-2  rounded-md text-[#6c00a5] shadow-xl p-2 "  key={index}>
+            <button onClick={() => {setdata(item.url) 
+              showbtn(item.title)
+              }}>{item.title}</button>
           </div>
         ))}
       </div>
@@ -54,17 +74,19 @@ export default function PlayList({props}:Play) {
       {/*  #2F0743 */}
 
     
-      <div className="mt-[15px]">
+      <div className="mt-3">
       
-      <h1 className="text-lg font-extrabold text-gray-900 -pt-[30px] dark:text-black md:text-5xl lg:text-4xl text-center"><span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-500 ">{props}</span>&nbsp;<span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Course</span></h1> 
+      <h1 className="text-lg font-extrabold text-gray-900 -pt-[30px] dark:text-black md:text-5xl lg:text-4xl text-center"><span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-500 ">{props}</span>&nbsp;<span className="underline underline-offset-4 decoration-6  decoration-[#fed32e] text-[#6c00a5]">Course</span></h1> 
 
-        <iframe className="rounded-2xl drop-shadow-2xl "
+        <iframe className="mt-2 rounded-2xl drop-shadow-2xl "
           width={820}
           height={500}
           src={list}
           allowFullScreen
           allow="poster"
         ></iframe>
+
+<button className="btn btn-outline btn-primary transition ease-in duration-300 mt-5" disabled={show} >Start Quiz</button>
       </div>
 
     
