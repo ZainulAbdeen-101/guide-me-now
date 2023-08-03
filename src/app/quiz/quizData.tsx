0ff19eefa58f3025d97256096d952ba59804e946
Abdul Hadi <abhadi56789@gmail.com>
2023-08-03 16:04:40 +0500

    quizzzzzzText

--- src/app/quiz/quizData.tsx
+++ src/app/quiz/quizData.tsx
@@ -5,48 +5,161 @@ const frontend_basic_quiz =[
 
 
 // HTML
-    
-    [
-                  
-              {
-                  question:"What does HTML stand for?",
-                  options:[
-                  
-                      {id:0,ans:"Hyperlinks and Text Markup Language",isCorrect:false},
-                      {id:1,ans:"Hyper Text Markup Language",isCorrect:true},
-                      {id:2,ans:"Home Tool Markup Language",isCorrect:false},
-                      {id:3,ans:"Hyper Tool Markup Language",isCorrect:false}
-                      
-                  ]
-              }
-              ,
-              
-              {
-                  question:"Which HTML tag is used to define a hyperlink?",
-                  options:[
-                  
-                      {id:0,ans:"<link>",isCorrect:false},
-                      {id:1,ans:"<href>",isCorrect:false},
-                      {id:2,ans:"<a>",isCorrect:true},
-                      {id:3,ans:"<hyperlink>",isCorrect:false}
-                      
-                  ]
-              },
-              
-              {
-                  question:"What is the correct way to create a heading level 1 (h1) in HTML?",
-                  options:[
-                  
-                      {id:0,ans:"<heading>",isCorrect:false},
-                      {id:1,ans:"<h1>",isCorrect:true},
-                      {id:2,ans:"<head>",isCorrect:false},
-                      {id:3,ans:"<h>",isCorrect:false}
-                      
-                  ]
-              } ,
-                 
-          
-              ]
+[
+    {
+      question: "What does HTML stand for?",
+      options: [
+        { id: 0, ans: "Hyper Text Markup Language", isCorrect: true },
+        { id: 1, ans: "Hyperlinks and Text Markup Language", isCorrect: false },
+        { id: 2, ans: "Home Tool Markup Language", isCorrect: false },
+        { id: 3, ans: "Hyper Tool Markup Language", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML tag is used to define a hyperlink?",
+      options: [
+        { id: 0, ans: "<a>", isCorrect: true },
+        { id: 1, ans: "<link>", isCorrect: false },
+        { id: 2, ans: "<href>", isCorrect: false },
+        { id: 3, ans: "<hyperlink>", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is the correct way to create a heading level 1 (h1) in HTML?",
+      options: [
+        { id: 0, ans: "<h1>", isCorrect: true },
+        { id: 1, ans: "<heading>", isCorrect: false },
+        { id: 2, ans: "<head>", isCorrect: false },
+        { id: 3, ans: "<h>", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML tag is used to add an image to a webpage?",
+      options: [
+        { id: 0, ans: "<img>", isCorrect: true },
+        { id: 1, ans: "<image>", isCorrect: false },
+        { id: 2, ans: "<picture>", isCorrect: false },
+        { id: 3, ans: "<src>", isCorrect: false },
+      ],
+    },
+    {
+      question: "How do you create a line break in HTML?",
+      options: [
+        { id: 0, ans: "<br>", isCorrect: true },
+        { id: 1, ans: "<break>", isCorrect: false },
+        { id: 2, ans: "<lb>", isCorrect: false },
+        { id: 3, ans: "<newline>", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is the correct HTML element for inserting a horizontal rule?",
+      options: [
+        { id: 0, ans: "<hr>", isCorrect: true },
+        { id: 1, ans: "<line>", isCorrect: false },
+        { id: 2, ans: "<hrule>", isCorrect: false },
+        { id: 3, ans: "<break>", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML attribute is used to specify the URL of the linked resource?",
+      options: [
+        { id: 0, ans: "href", isCorrect: true },
+        { id: 1, ans: "src", isCorrect: false },
+        { id: 2, ans: "link", isCorrect: false },
+        { id: 3, ans: "url", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is the purpose of the <meta> tag in HTML?",
+      options: [
+        { id: 0, ans: "To specify metadata about an HTML document", isCorrect: true },
+        { id: 1, ans: "To define a paragraph of text", isCorrect: false },
+        { id: 2, ans: "To create a list of items", isCorrect: false },
+        { id: 3, ans: "To define a table in HTML", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML element is used to define a table row?",
+      options: [
+        { id: 0, ans: "<tr>", isCorrect: true },
+        { id: 1, ans: "<td>", isCorrect: false },
+        { id: 2, ans: "<table>", isCorrect: false },
+        { id: 3, ans: "<th>", isCorrect: false },
+      ],
+    },
+    {
+      question: "How do you create an unordered list in HTML?",
+      options: [
+        { id: 0, ans: "<ul>", isCorrect: true },
+        { id: 1, ans: "<list>", isCorrect: false },
+        { id: 2, ans: "<ol>", isCorrect: false },
+        { id: 3, ans: "<unordered>", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is the correct way to add a comment in HTML?",
+      options: [
+        { id: 0, ans: "<!-- This is a comment -->", isCorrect: true },
+        { id: 1, ans: "// This is a comment", isCorrect: false },
+        { id: 2, ans: "/* This is a comment */", isCorrect: false },
+        { id: 3, ans: "<comment>This is a comment</comment>", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML attribute is used to define inline styles for an element?",
+      options: [
+        { id: 0, ans: "style", isCorrect: true },
+        { id: 1, ans: "css", isCorrect: false },
+        { id: 2, ans: "class", isCorrect: false },
+        { id: 3, ans: "inline", isCorrect: false },
+      ],
+    },
+    {
+      question: "What does the <iframe> element in HTML do?",
+      options: [
+        { id: 0, ans: "Defines an inline frame for embedding external content", isCorrect: true },
+        { id: 1, ans: "Creates a floating image on the webpage", isCorrect: false },
+        { id: 2, ans: "Defines an image gallery", isCorrect: false },
+        { id: 3, ans: "Defines a video player", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML element is used to define a navigation menu?",
+      options: [
+        { id: 0, ans: "<nav>", isCorrect: true },
+        { id: 1, ans: "<navigation>", isCorrect: false },
+        { id: 2, ans: "<menu>", isCorrect: false },
+        { id: 3, ans: "<navbar>", isCorrect: false },
+      ],
+    },
+    {
+      question: "How do you create a checkbox input in HTML?",
+      options: [
+        { id: 0, ans: "<input type=\"checkbox\">", isCorrect: true },
+        { id: 1, ans: "<checkbox>", isCorrect: false },
+        { id: 2, ans: "<input type=\"input\">", isCorrect: false },
+        { id: 3, ans: "<check>", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is the correct HTML element for inserting a video on a webpage?",
+      options: [
+        { id: 0, ans: "<video>", isCorrect: true },
+        { id: 1, ans: "<movie>", isCorrect: false },
+        { id: 2, ans: "<media>", isCorrect: false },
+        { id: 3, ans: "<vid>", isCorrect: false },
+      ],
+    },
+    {
+      question: "Which HTML element is used to define the document's header section?",
+      options: [
+        { id: 0, ans: "<head>", isCorrect: true },
+        { id: 1, ans: "<header>", isCorrect: false },
+        { id: 2, ans: "<top>", isCorrect: false },
+        { id: 3, ans: "<head-section>", isCorrect: false },
+      ],
+    },
+  ]
         ,
 
 
@@ -139,6 +252,8 @@ const frontend_basic_quiz =[
 
 
 
+
+  
 
 
 export function getQuizData_current(head:string){
