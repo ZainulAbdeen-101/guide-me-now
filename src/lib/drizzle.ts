de4029067bd68e2eb4dc4c742028101c7b4e5aea
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-08-04 23:50:39 +0500

    done

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,16 +1,27 @@
-// import {pgTable,varchar,integer,boolean,serial} from 'drizzle-orm/pg-core'
-// import {drizzle} from 'drizzle-orm/vercel-postgres'
-// import {sql} from '@vercel/postgres'
+import {pgTable,varchar,integer,boolean,serial} from 'drizzle-orm/pg-core'
+import {drizzle} from 'drizzle-orm/vercel-postgres'
+import {sql} from '@vercel/postgres'
 
 
-//  export const Quizdata=pgTable("quiz",{
-// id:serial('id').primaryKey(),
-// heading:varchar("heading").notNull(),
-// marks:integer('marks').notNull(),
-// correct:boolean('correct').notNull()
+ export const Quizdata=pgTable("quiz",{
+id:serial('id').primaryKey(),
+heading:varchar("heading").notNull(),
+imageurl:varchar('imageurl').notNull(),
+marks:integer('marks').notNull(),
+correct:boolean('correct').notNull()
 
-// })
+})
 
-//    export const db=drizzle(sql)
+export const profiledata=pgTable("profile",{
+   id:serial('id').primaryKey(),
+   userid:varchar("userid").notNull().unique(),
+   about:varchar('about').notNull(),
+   education:varchar('education').notNull(),
+   skills:varchar('skills').notNull()
+
+
+})
+
+   export const db=drizzle(sql)
 
 
